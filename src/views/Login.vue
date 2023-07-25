a81c1585e4f080914f02a4be194cb7ddeb33c33a
Zain Ul Abaiden <76576809+Zain-Ul-Abaiden@users.noreply.github.com>
2023-07-25 09:41:43 +0500

    projrct

---
+++ src/views/Login.vue
@@ -0,0 +1,80 @@
+<template>
+<form @submit.prevent="handleSubmit">
+    <label>Email: </label>
+    <input type="text" placeholder="Enter email here" required v-model="email" />
+    <br /> <br />
+
+    <label>Password: </label>
+    <input type="password" placeholder="Enter password here" required v-model="password" />
+    <br /> <br />
+
+    <div class="submit">
+            <button>Login</button>
+    </div>
+
+</form>
+  </template>
+
+<script>
+export default{
+    data() {
+        return {
+            email: '',
+            password: '',
+            passwordError: ''
+        }
+    },
+    methods: {
+        handleSubmit(){
+          //validate password
+          this.passwordError = this.password.length > 7 ? '' : 'Passwor must be at least 8 chars long'
+        }
+    }
+}
+</script>
+
+<style>
+form {
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+}
+label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+}
+input {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+}
+button {
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+}
+.submit {
+    text-align: center;
+}
+.error {
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
+}
+</style>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,90 @@
+<template>
+    <form @submit.prevent="handleSubmit">
+        <label>Name:</label>
+        <input type="text" placeholder="Enter name here" required v-model="name">
+
+        <label>Email:</label>
+        <input type="email" placeholder="Enter email here" required v-model="email">
+    
+        <label>Password:</label>
+        <input type="password" placeholder="Enter password here" required v-model="password">
+        <div v-if="passwordError" class="error"> {{ passwordError }} </div>
+
+        <label>Confirm Password:</label>
+        <input type="password" placeholder="Enter again password" required v-model="confirmpassword">
+        <!-- <div v-if="confirmpassword" class="error"> {{ confirmpassword }} </div> -->
+    
+        <div class="submit">
+            <button>Create an Account</button>
+        </div>
+        
+    </form>
+    <!-- <p>Email: {{ email }}</p>
+    <p>Password: {{ password }}</p> -->
+    </template>
+    
+    <script>
+    export default{
+        data() {
+            return {
+                name: '',
+                email: '',
+                password: '',
+                passwordError: '',
+                confirmpassword: ''
+            }
+        },
+        methods: {
+            handleSubmit(){
+              //validate password
+              this.passwordError = this.password.length > 7 ? '' : 'Password must be at least 8 chars long';
+              this.confirmpassword === this.password ? '': 'Password did not match!'
+            }
+        }
+    }
+    </script>
+    <style>
+    form {
+        max-width: 420px;
+        margin: 30px auto;
+        background: white;
+        text-align: left;
+        padding: 40px;
+        border-radius: 10px;
+    }
+    label {
+        color: #aaa;
+        display: inline-block;
+        margin: 25px 0 15px;
+        font-size: 0.6em;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+        font-weight: bold;
+    } 
+    input {
+        display: block;
+        padding: 10px 6px;
+        width: 100%;
+        box-sizing: border-box;
+        border: none;
+        border-bottom: 1px solid #ddd;
+        color: #555;
+    }
+    button {
+        background: #0b6dff;
+        border: 0;
+        padding: 10px 20px;
+        margin-top: 20px;
+        color: white;
+        border-radius: 20px;
+    }
+    .submit {
+        text-align: center;
+    }
+    .error {
+        color: #ff0062;
+        margin-top: 10px;
+        font-size: 0.8em;
+        font-weight: bold;
+    }
+    </style>
